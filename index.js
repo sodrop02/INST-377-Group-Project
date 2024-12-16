29f20c51e6b8f39a21448e7c676e836d2738d065
Josh Hu <joshua.hu1337@gmail.com>
2024-12-15 21:18:10 -0500

    bruh

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const port = process.env.PORT || 3000;
 // Middleware for parsing JSON
 app.use(express.json());
 app.use(cors({
-    origin: 'http://127.0.0.1:3000', // Replace with your frontend URL
+    origin: [
+        'http://127.0.0.1:3000', // Local development
+        'https://inst-377-group-project-ten.vercel.app' // Deployed Vercel frontend
+    ],
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
