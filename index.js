b6a36770f23830df0c38dc788abb940fd510edc6
Josh Hu <joshua.hu1337@gmail.com>
2024-12-16 14:56:37 -0500

    bruh

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-
+console.log("backend running")
 // Middleware for parsing JSON
 app.use(express.json());
 
